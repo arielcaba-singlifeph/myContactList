bd6dc05aa9246de51f8cc26473ab4d8d1245a5ec
mcariel <mcarielcaba@gmail.com>
2016-03-26 22:21:03 +0800

    modified:   app.js

--- app.js
+++ app.js
@@ -11,21 +11,15 @@ var users = require('./routes/users');
 var api = require('./routes/api');
 var contacts = require('./routes/contacts');
 
-var db_name = mycontactlist;
-var mongodb_connection_string = '';
-//provide a sensible default for local development
-mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
-//take advantage of openshift env vars when available:
-if(process.env.OPENSHIFT_MONGODB_DB_URL){
-  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
-}
+
 // connect to mongoDB
-mongoose.connect(mongodb_connection_string);
+mongoose.connect('mongodb://localhost/contacts');
 var db = mongoose.connection;
 db.on('error', console.error.bind('console', 'connection error'));
 db.once('open', function(){
   console.log('We are connected to mongoDB!');
 });
+
 
 var app = express();
 
