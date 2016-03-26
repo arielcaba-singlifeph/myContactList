3892a4b9580f6b2ab64a3c6322e9e8dfe46fc13c
mcariel <mcarielcaba@gmail.com>
2016-03-26 22:23:16 +0800

    modified:   app.js

--- app.js
+++ app.js
@@ -72,13 +72,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
-
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-
-app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", server_port " + port )
-});
+app.listen(8080);
+console.log('Server is listening on http://localhost:8000');
 
 module.exports = app;
